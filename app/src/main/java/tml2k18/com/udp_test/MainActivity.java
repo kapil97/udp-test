61268f206ce76d04d970d01241394ab2efd63bcc
kapil97 <kamleshwar.kamleshwar15@siesgst.ac.in>
2018-02-10 11:01:21 +0530

    very initial

---
+++ app/src/main/java/tml2k18/com/udp_test/MainActivity.java
@@ -0,0 +1,60 @@
+package tml2k18.com.udp_test;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.net.SocketException;
+
+public class MainActivity extends AppCompatActivity {
+    Button b1;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+         b1=findViewById(R.id.btn1);
+        b1.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                       Tfunc();
+
+                    }
+                }
+        );
+
+    }
+    public void Tfunc(){
+        Runnable ru=new Runnable() {
+            @Override
+            public void run() {
+                int port=5000;
+                String ip="192.168.137.227";
+                try {
+                    DatagramSocket udpSocket = new DatagramSocket(port);
+                    InetAddress serverAddr = InetAddress.getByName(ip);
+                    byte[] buf = ("1").getBytes();
+                    DatagramPacket packet = new DatagramPacket(buf, buf.length,serverAddr, port);
+                    udpSocket.send(packet);
+                } catch (SocketException e) {
+                    Log.e("Udp:", "Socket Error:", e);
+                } catch (IOException e) {
+                    Log.e("Udp Send:", "IO Error:", e);
+                }
+            }
+        };
+        Thread abc=new Thread(ru);
+        abc.start();
+    }
+
+
+}
+
+
+
