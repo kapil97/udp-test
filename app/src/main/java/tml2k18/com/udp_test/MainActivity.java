1179a9d511d0709bf7e923c9c1d83a9bf778f6e7
kapil97 <kamleshwar.kamleshwar15@siesgst.ac.in>
2018-02-14 17:05:32 +0530

    added sensors values

--- app/src/main/java/tml2k18/com/udp_test/MainActivity.java
+++ app/src/main/java/tml2k18/com/udp_test/MainActivity.java
@@ -1,11 +1,13 @@
 package tml2k18.com.udp_test;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.io.IOException;
 import java.net.DatagramPacket;
@@ -14,29 +16,52 @@ import java.net.InetAddress;
 import java.net.SocketException;
 
 public class MainActivity extends AppCompatActivity {
-    Button b1;
-    TextView t1,t2;
-    String ipAdd,val1;
-    int val;
+    Button b1,b2;
+    TextView t1,t2,t3;
+    String ipAdd,val1,totalval;
+    int val=0;
+    int tval;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         b1=findViewById(R.id.btn1);
+        b2=findViewById(R.id.btn2);
         t1=findViewById(R.id.txt1);
         t2=findViewById(R.id.txt2);
+        t3=findViewById(R.id.txt3);
         b1.setOnClickListener(
                 new View.OnClickListener() {
                     @Override
                     public void onClick(View v) {
                         ipAdd=t1.getText().toString();
+
                         val1=t2.getText().toString().trim();
-                        val=Integer.parseInt(val1);
-                        Tfunc(ipAdd,val);
+                        if((val1.equals(""))){
+                         Toast t1=Toast.makeText(MainActivity.this,"Enter valid number plis",Toast.LENGTH_SHORT);
+                         t1.show();
+
+                        }
+                        else {
+                            val = Integer.parseInt(val1);
+                            Tfunc(ipAdd,val);
+                        }
 
                     }
                 }
         );
+        b2.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        totalval=getIntent().getStringExtra("val");
+                        tval=Integer.parseInt(totalval);
+                        Tfunc(ipAdd,tval);
+                        t3.setText(totalval);
+                    }
+                }
+        );
+
 
     }
     public void Tfunc(String a,int n){
